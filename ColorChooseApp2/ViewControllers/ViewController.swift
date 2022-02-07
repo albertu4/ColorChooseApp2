73a07f5f6709909a9386ed6a895ff6dca94f10cf
Miwka <97275239+albertu4@users.noreply.github.com>
2022-02-07 23:38:55 +0900

    Initial Commit

---
+++ ColorChooseApp2/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorChooseApp2
+//
+//  Created by Михаил Иванов on 07.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
