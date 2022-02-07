a3c67507c6c9fad7ffe6d1050bc18d5601236f2f
Miwka <97275239+albertu4@users.noreply.github.com>
2022-02-08 00:12:31 +0900

    Made MainVC

---
+++ ColorChooseApp2/ViewControllers/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  ColorChooseApp2
+//
+//  Created by Михаил Иванов on 08.02.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
